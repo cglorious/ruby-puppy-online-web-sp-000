e2bebfd45b31fe2cdb6578005f6a529a9e917ba5
Catherine Gloria <catherinegloria@gmail.com>
2020-10-01 21:01:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+class Dog
+
+  @@all = [] #objects of dogs
+  #@@print_all = []
+
+  attr_accessor :name, :save
+
+  def initialize(name)
+    @name = name
+    save
+    #@@print_all << name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    puts @@all
+    #@@all.each do |e|
+    #  puts @name
+    #end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
